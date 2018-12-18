4ea19da24b0db4c46acdb82c87ed33f780910478
Jonas <jwallat@hotmail.de>
2018-12-18 09:38:09 +0100

    test

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -1,13 +1,17 @@
-import "dart:convert";
 import "package:sqflite/sqflite.dart";
 import "package:path_provider/path_provider.dart";
 import "dart:io";
 import "package:path/path.dart";
 import "dart:async";
 import "../models/item_model.dart";
+import "repository.dart";
 
-class NewsDbProvider {
+class NewsDbProvider implements Source, Cache {
   Database db;
+
+  NewsDbProvider() {
+    init();
+  }
 
   void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -38,6 +42,11 @@ class NewsDbProvider {
     );
   }
 
+  // Todo store and fetch top ids
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
+
   Future<ItemModel> fetchItem(int id) async {
     final maps = await db.query(
       "Items",
@@ -56,3 +65,5 @@ class NewsDbProvider {
     return db.insert("Items", item.toMapForDb());
   }
 }
+
+final newsDbProvider = NewsDbProvider();
