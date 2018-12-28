7a5491649d0281f14058eb7914996b16ba7e2237
jwallat <jwallat@hotmail.de>
2018-12-28 17:55:17 +0100

    list added

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -62,7 +62,14 @@ class NewsDbProvider implements Source, Cache {
   }
 
   Future<int> addItem(ItemModel item) {
-    return db.insert("Items", item.toMapForDb());
+    return db.insert(
+      "Items",
+      item.toMapForDb(), 
+      conflictAlgorithm: ConflictAlgorithm.ignore,
+      );
+  }
+  Future<int> clear() {
+    return db.delete("Items");
   }
 }
 
--- news/lib/src/resources/repository.dart
+++ news/lib/src/resources/repository.dart
@@ -18,7 +18,7 @@ class Repository {
 
   Future<ItemModel> fetchItem(int id) async {
     ItemModel item;
-    Source source;
+    var source;
     for (source in sources) {
       item = await source.fetchItem(id);
       if (item != null) {
@@ -27,11 +27,20 @@ class Repository {
     }
 
     for (Cache cache in caches) {
-      cache.addItem(item);
+      if (cache != source) {
+        cache.addItem(item);
+      }
     }
 
     return item;
   }
+
+  clearCache() async {
+    for (var cache in caches) {
+      await cache.clear();
+    }
+  }
+
 }
 
 abstract class Source {
@@ -41,4 +50,5 @@ abstract class Source {
 
 abstract class Cache {
   Future<int> addItem(ItemModel item);
+  Future<int> clear();
 }
